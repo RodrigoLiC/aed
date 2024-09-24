8b59829b4e16c926eff70805a08ed5a4b8f81f47
RodrigoLiC <rodrigo.li@utec.edu.pe>
2024-09-24 12:29:22 -0500

    tree

--- clases/binaryTree/binaryTree.cpp
+++ clases/binaryTree/binaryTree.cpp
@@ -31,6 +31,55 @@ private:
         }
     }
 
+    Node* auxRemove(Node* current, T value) {
+        if (current == nullptr) return current;
+        // search
+        if (value < current->data) {
+            current->left = auxRemove(current->left, value);
+        } else if (value > current->data) {
+            current->right = auxRemove(current->right, value);
+        }
+        // current->data == value
+        else {
+            // no children
+            if (current->left == nullptr && current->right == nullptr) {
+                delete current;
+                return nullptr;
+            }
+            // one child
+            else if (current->left == nullptr) {
+                Node* temp = current->right;
+                delete current;
+                return temp;
+            } else if (current->right == nullptr) {
+                Node* temp = current->left;
+                delete current;
+                return temp;
+            }
+            // two children
+            else {
+                Node* temp_parent = current;
+                Node* temp = current->left;
+                while (temp->right != nullptr) {
+                    temp_parent = temp;
+                    temp = temp->right;
+                }
+                if (temp_parent == current) {
+                    temp->right = current->right;
+                    delete current;
+                    return temp;
+                } else {
+                    temp_parent->right = temp->left;
+                    temp->left = current->left;
+                    temp->right = current->right;
+                    delete current;
+                    return temp;
+                }
+            }
+        }
+        return current;
+    }
+
     void auxClear(Node* node) {
         if (node != nullptr) {
             auxClear(node->left);
@@ -45,7 +94,6 @@ private:
         int right = auxHeight(node->right);
         return 1 + max(left, right);
     }
-
 
     void preOrder(Node* node) const {
         if (node == nullptr) return;
@@ -68,33 +116,6 @@ private:
         cout << node->data << " ";
     }
 
-
-public:
-    BinaryTree() : root(nullptr) {}
-    ~BinaryTree() { auxClear(root); }
-
-    void insert(T value) {
-        if (root == nullptr) {
-            root = new Node(value);
-            return;
-        }
-        auxInsert(root, value);
-    }
-
-    int height() {
-        return auxHeight(root);
-    }
-
-    void preOrderTraversal() const {
-        preOrder(root);
-    }
-    void inOrderTraversal() const {
-        inOrder(root);
-    }
-    void postOrderTraversal() const {
-        postOrder(root);
-    }
-
     void drawCurrentDepth(Node* node, int depth, int spaces) {
         if (node == nullptr) {
             if (depth == 0) {
@@ -115,6 +136,55 @@ public:
         }
         drawCurrentDepth(node->left, depth - 1, spaces);
         drawCurrentDepth(node->right, depth - 1, spaces);
+    }
+
+public:
+    BinaryTree() : root(nullptr) {}
+    ~BinaryTree() { auxClear(root); }
+
+    void insert(T value) {
+        if (root == nullptr) {
+            root = new Node(value);
+            return;
+        }
+        auxInsert(root, value);
+    }
+
+    Node* search(T value) {
+        Node* current = root;
+        while (current != nullptr) {
+            if (current->data == value) {
+                return current;
+            } else if (value < current->data) {
+                current = current->left;
+            } else {
+                current = current->right;
+            }
+        }
+        return nullptr;
+    }
+
+    void remove(T value) {
+        root = auxRemove(root, value);
+    }
+
+    void clear() {
+        auxClear(root);
+        root = nullptr;
+    }
+
+    int height() {
+        return auxHeight(root);
+    }
+
+    void preOrderTraversal() const {
+        preOrder(root);
+    }
+    void inOrderTraversal() const {
+        inOrder(root);
+    }
+    void postOrderTraversal() const {
+        postOrder(root);
     }
 
     void draw() {
@@ -149,17 +219,44 @@ int main() {
 
     tree.draw();
 
-    BinaryTree<int> tree2;
-    tree2.insert(1);
-    tree2.insert(2);
-    tree2.insert(3);
-    tree2.insert(4);
-
-    cout << "inOrderTraversal: ";
-    tree2.inOrderTraversal();
-    cout << "\n";
-    tree2.draw();
-
+    cout << "insert (i)\tremove (r)\tsearch (s)\theight (h)\ttraverse (t)\t\texit (e)\n";
+    char option;
+    while (cin >> option) {
+        if (option == 'i') {
+            int value;
+            cin >> value;
+            tree.insert(value);
+            tree.draw();
+        } else if (option == 'r') {
+            int value;
+            cin >> value;
+            tree.remove(value);
+            tree.draw();
+        } else if (option == 's') {
+            int value;
+            cin >> value;
+            if (tree.search(value) != nullptr) {
+                cout << "Found\n";
+            } else {
+                cout << "Not found\n";
+            }
+        } else if (option == 'h') {
+            cout << "Height: " << tree.height() << "\n";
+        } else if (option == 't') {
+            cout << "Pre-order Traversal: ";
+            tree.preOrderTraversal();
+            cout << "\n";
+            cout << "In-order Traversal: ";
+            tree.inOrderTraversal();
+            cout << "\n";
+            cout << "Post-order Traversal: ";
+            tree.postOrderTraversal();
+            cout << "\n";
+        } else if (option == 'e') {
+            break;
+        }
+        cout << "insert (i)\t\tremove (r)\t\tsearch (s)\t\theight (h)\t\texit (e)\n";
+    }
 
     return 0;
 }
